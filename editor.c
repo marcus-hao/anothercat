321c8bf53657d2f3411a8ba3d3827c88a53e874b
Marcus Chan <24224821+marcus-hao@users.noreply.github.com>
2022-09-07 01:06:37 +0800

    add: cursor snapping to end of line The cursor snaps to the beginning of next line or end of previous line depending on the arrow keys input.

--- editor.c
+++ editor.c
@@ -310,11 +310,17 @@ void editorMoveCursor(int key) {
         case ARROW_LEFT:
             if (E.cx != 0) {
                 E.cx--;
+            } else if (E.cy > 0) {
+                E.cy--;
+                E.cx = E.row[E.cy].size;
             }
             break;
         case ARROW_RIGHT:
             if (row && E.cx < row->size) {
                 E.cx++;
+            } else if (row && E.cx == row->size) {
+                E.cy++;
+                E.cx = 0;
             }
             break;
         case ARROW_UP:
@@ -328,6 +334,11 @@ void editorMoveCursor(int key) {
             }
             break;
         
+    }
+    row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+    int rowlen = row ? row->size : 0;
+    if (E.cx > rowlen) {
+        E.cx = rowlen;
     }
 }
 
