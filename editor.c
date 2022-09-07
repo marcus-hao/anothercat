96e18b51ce0ea7fabdfb6e84c04b809a41ee6408
Marcus Chan <24224821+marcus-hao@users.noreply.github.com>
2022-09-07 14:39:37 +0800

    Add keypresses for scrolling Page scrolling with PageUp and PageDown keys. Cursor positioning with Home and End keys.

--- editor.c
+++ editor.c
@@ -407,12 +407,19 @@ void editorProcessKeyPress() {
             break;
         
         case END_KEY:
-            E.cx = E.screencols - 1;
+            if (E.cy < E.numrows)
+                E.cx = E.row[E.cy].size;
             break;
 
         case PAGE_UP:
         case PAGE_DOWN:
             {
+                if ( c == PAGE_UP) {
+                    E.cy = E.rowoff;
+                } else if (c == PAGE_DOWN) {
+                    E.cy = E.rowoff + E.screencols - 1;
+                    if (E.cy > E.numrows) E.cy = E.numrows;
+                }
                 int times = E.screenrows;
                 while (times--)
                     editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
