ebd060159344c705261607890d4e350409fd7f27
Marcus Chan <24224821+marcus-hao@users.noreply.github.com>
2022-12-21 09:04:59 +0800

    small changes to name

--- editor.c
+++ editor.c
@@ -311,7 +311,7 @@ void editorDrawRows(struct abuf *ab) {
             if (E.numrows == 0 && y == E.screenrows / 3) {
                 char welcome[80];
                 int welcomelen = snprintf(welcome, sizeof(welcome), 
-                "Kilo editor -- version %s", KILO_VERSION);
+                "AnotherCat -- version %s", KILO_VERSION);
                 if (welcomelen > E.screencols) welcomelen = E.screencols;
                 int padding = (E.screencols - welcomelen) / 2;
                 if (padding) {
